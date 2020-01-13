e7464b2fdf2a8e064b7681afb00bdb3d2c644362
Anisha Jayan <>
2020-01-14 01:50:48 +0530

    fixes

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -10,7 +10,7 @@ angular.module('NarrowItDownApp', [])
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
         var narrowItDownCtrl = this;
-        narrowItDownCtrl.found = [{name: "abc", short_name: "a", description: "desc"}]
+        narrowItDownCtrl.found = []
         narrowItDownCtrl.searchTerm = ""
         narrowItDownCtrl.search = function(searchTerm){
                 var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
