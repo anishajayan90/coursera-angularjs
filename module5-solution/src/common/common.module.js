d493f462df79b02b4f5cf339b42b4aeac43e3955
Anisha Jayan <>
2020-02-01 20:20:32 +0530

    added own heroku server url

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://restro-menu-items.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
