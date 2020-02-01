54ece42cc6fc1e6ae18bb72094f391dda20af538
Anisha Jayan <>
2020-02-01 19:09:45 +0530

    added more

--- module5-solution/src/public/public.routes.js
+++ module5-solution/src/public/public.routes.js
@@ -21,7 +21,14 @@ function routeConfig ($stateProvider) {
     })
     .state('public.myinfo', {
       url: '/myinfo',
-      templateUrl: 'src/public/home/myinfo.html'
+      templateUrl: 'src/public/myinfo/myinfo.html',
+      controller: 'MyInfoController',
+      controllerAs: 'myInfoCtrl',
+      resolve: {
+        info: ['MyInfoService', function(MyInfoService) {
+          return MyInfoService.getInfo();
+        }]
+      }
     })
     .state('public.signup', {
       url: '/signup',
--- module5-solution/src/public/registration/registration.controller.js
+++ module5-solution/src/public/registration/registration.controller.js
@@ -4,9 +4,36 @@
 angular.module('public')
 .controller('RegistrationController', RegistrationController);
 
-function RegistrationController() {
+RegistrationController.$inject = ['MenuService', 'MyInfoService'];
+function RegistrationController(MenuService, MyInfoService) {
   var $ctrl = this;
-  // $ctrl.menuCategories = menuCategories;
+  $ctrl.info = {};
+
+  $ctrl.submit = function() {
+      MenuService.getMenuItem($ctrl.info.favorite)
+        .then(function(response) {
+          $ctrl.invalidFavorite = false;
+          $ctrl.submitted = true;
+          MyInfoService.setInfo($ctrl.info);
+        })
+        .catch(function() {
+          $ctrl.invalidFavorite = true;
+        });
+
+
+    }
+
+    $ctrl.validateFavorite = function() {
+      MenuService.getMenuItem($ctrl.info.favorite)
+        .then(function () {
+          $ctrl.invalidFavorite = false;
+        })
+        .catch(function() {
+          $ctrl.invalidFavorite = true;
+        });
+    }
+
+  };
 }
 
 
